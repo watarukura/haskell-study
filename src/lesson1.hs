5e9e6e361b97e20e276a1017ff1e34b11a02026e
wataru-kurashima <wataru-kurashima@hands-lab.com>
2021-02-18 13:01:35 +0900

    chore: delete duplicate

--- src/lesson1.hs
+++ src/lesson1.hs
@@ -1,25 +1,6 @@
 -- main = do
 --     print "Hello, World!"
 
-messyMain :: IO()
-messyMain = do
-    print "Who is the email for?"
-    recipient <- getLine 
-    print "What is the Title?"
-    title <- getLine 
-    print "Who is the Author?"
-    author <- getLine 
-    print ("Dear " ++ recipient ++ ",\n" ++
-           "THanks for buying " ++ title ++ "\nthanks,\n" ++ author)
-
-toPart recipient = "Dear " ++ recipient ++ ",\n"
-
-bodyPart bookTitle = "Thanks for buying " ++ bookTitle ++ ".\n"
-fromPart author = "Thanks,\n" ++ author
-createEmail recipient bookTitle author = toPart recipient ++
-                                         bodyPart bookTitle ++
-                                         fromPart author
-
 main = do
     print "Who is the email for?"
     recipient <- getLine 
