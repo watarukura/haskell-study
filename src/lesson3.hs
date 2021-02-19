5c1444a198903f18dad7809561c815e43d46b04f
wataru-kurashima <wataru-kurashima@hands-lab.com>
2021-02-19 13:21:44 +0900

    style: fix by hlint

--- src/lesson3.hs
+++ src/lesson3.hs
@@ -12,7 +12,8 @@ sumSquareOrSquareSum x y = let sumSquare = x^2 + y^2
                             then sumSquare
                             else squareSum
 
-doubleDouble x = (\dubs -> dubs*2) (x*2)
+-- doubleDouble x = (\dubs -> dubs*2) (x*2)
+doubleDouble x = (* 2) (x*2)
 
 -- overwrite x = let x = 2
 --               in
@@ -21,12 +22,14 @@ doubleDouble x = (\dubs -> dubs*2) (x*2)
 --                   let x = 4
 --                   in
 --                     x
-overwrite x = (\x ->
-    (\x ->
-        (\x -> x) 4
-    ) 3
-  ) 2
+-- overwrite x = (\x ->
+--     (\x ->
+--         (\x -> x) 4
+--     ) 3
+--   ) 2
+overwrite x = 4
 
-counter x = (\x ->
-    (\x -> x + 1) x
-  ) x
+-- counter x = (\x ->
+--     (\x -> x + 1) x
+--   ) x
+counter = (+ 1)
