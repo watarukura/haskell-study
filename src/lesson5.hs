ad743849c4de99271bfa543105505e3ec938dc87
watarukura <what.r.j150@gmail.com>
2021-03-19 13:16:08 +0900

    style: 💄 fix by hlint

--- src/lesson5.hs
+++ src/lesson5.hs
@@ -14,15 +14,10 @@ getRequestUrl host apikey resource id = host ++
 
 exampleUrlBuilder = getRequestUrl "http://example.com"
 
-genApiRequestBuilder hostBuilder apiKey = (\resouce id ->
-                                            hostBuilder apiKey resouce id)
+genApiRequestBuilder hostBuilder = hostBuilder
 
 myExampleUrlBuilder = genApiRequestBuilder exampleUrlBuilder "i337hAsk311"
 
 myExampleUrlBuilder2 = genApiRequestBuilder exampleUrlBuilder "i337hAsk311" "book"
 
 add4 a b c d = a + b + c + d
-addXto3 x = (\b c d ->
-                add4 x b c d)
-addXto2 x y = (\c d ->
-                add4 x y c d)
